8bcf011dfee98bc2810016eb8f61429ddcf8869e
acro-yamaguchi <yamaguchi.hayato@acrovision.jp>
2021-12-18 21:23:39 +0900

    side-navi [add] 時計機能

--- side-navi/src/views/Time.vue
+++ side-navi/src/views/Time.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="time ma-n3">
-      <h1>Hello World</h1>
+      <h1>{{ now }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    
+    now: "",
   }),
+
+  mounted : function(){
+    this.getDate();
+  },
   
   methods: {
-    set2fig(num) {
+    getDate(){
+      // 今日の日付データをcurrentDateに格納
+      const currentDate = new Date();
+
+      // 年・月・日・曜日を取得
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1;
+      const date = currentDate.getDate();
+      const day = currentDate.getDay();
+      const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+      // 時間を取得
+      const hour = this.formatTime(currentDate.getHours());
+      const minute = this.formatTime(currentDate.getMinutes());
+      const second = this.formatTime(currentDate.getSeconds());
+      this.now = year + "/" + month + "/" + date  + "/" + weekday[day]  + "/ " + hour + ":" + minute + ":" + second;
+    },
+
+    formatTime(num) {
       // 桁数が1桁だったら先頭に0を加えて2桁に調整する
       var ret;
       if( num < 10 ) { ret = "0" + num; }
